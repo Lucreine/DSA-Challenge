986f2e803743acd62b00ce19c558f09a7749e6f5
Lucresse Reine ADANDE <adandelucressereine@gmail.com>
2024-06-24 10:09:39 +0100

    exercise 9 which talks about insertion and recovery of the prefix of a word

--- src/9_Trie/index.js
+++ src/9_Trie/index.js
@@ -1,5 +1,46 @@
-export class Trie {
-  insert(word) {}
-
-  retrieve(word) {}
+class Node {
+  constructor() {
+    this.children = {};
+    this.EndWord = false;
+  }
 }
+
+export class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(word) {
+    let currentNode = this.root;
+    for (const char of word) {
+      if (!currentNode.children[char]) {
+        currentNode.children[char] = new Node();
+      }
+      currentNode = currentNode.children[char];
+    }
+    currentNode.EndWord = true;
+  }
+
+  retrieve(prefix) {
+    let currentNode = this.root;
+    for (const char of prefix) {
+      if (!currentNode.children[char]) {
+        return null;
+      }
+      currentNode = currentNode.children[char];
+    }
+    return this._collectAllWords(currentNode, prefix);
+  }
+
+  _collectAllWords(node, prefix) {
+    const words = [];
+    if (node.EndWord) {
+      words.push(prefix);
+    }
+    for (const char in node.children) {
+      words.push(...this._collectAllWords(node.children[char], prefix + char));
+    }
+    return words;
+  }
+}
+
