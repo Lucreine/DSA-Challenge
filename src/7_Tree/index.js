499128b7d8f64d9244811d2e5cfd3a252a41e26e
Lucresse Reine ADANDE <adandelucressereine@gmail.com>
2024-06-24 09:23:09 +0100

    exercise 7 which talks about trees

--- src/7_Tree/index.js
+++ src/7_Tree/index.js
@@ -1,13 +1,55 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    this.children.push(newNode);
+    return newNode;
+  }
+}
+
 export class Tree {
-  // Return the newly inserted node/sub-tree
-  insert(value) {}
+  constructor() {
+    this.root = null;
+  }
 
-  // Breadth-first search
-  BFS(visitor) {}
+  insert(value) {
+    if (!this.root) {
+      this.root = new 
+      Node(value);
+      return this.root;
+    }
+    return this.root.insert(value);
+  }
 
-  // Pre-order depth-first search
-  preOrderDFS(visitor) {}
+  BFS(visitor) {
+    if (!this.root) return;
+    const queue = [this.root];
+    while (queue.length > 0) {
+      const node = queue.shift();
+      visitor(node.value);
+      for (const child of node.children) {
+        queue.push(child);
+      }
+    }
+  }
 
-  // Post-order depth-first search
-  postOrderDFS(visitor) {}
+  preOrderDFS(visitor, node = this.root) {
+    if (!node) return;
+    visitor(node.value);
+    for (const child of node.children) {
+      this.preOrderDFS(visitor, child);
+    }
+  }
+
+  postOrderDFS(visitor, node = this.root) {
+    if (!node) return;
+    for (const child of node.children) {
+      this.postOrderDFS(visitor, child);
+    }
+    visitor(node.value);
+  }
 }
